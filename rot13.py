5be683d3be36fa011da7a44c250cdd24f6e3b744
Andy Copland <andy.copland@gmail.com>
2020-01-24 18:33:25 +0000

    And now you have to finish it.

--- rot13.py
+++ rot13.py
@@ -1,7 +1,15 @@
 import string
-rot13 = string.maketrans( 
-    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
-    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+rot13 = str.maketrans( 
+    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz://.%?=-", 
+    "NOPQRSTUVWXYZnopqrstuvwxyz://.%?=-ABCDEFGHIJKLMabcdefghijklm")
 
-string.translate("Hello World!", rot13)
+str.translate("This used to be classed as encryption.", rot13)
 
+rot13-reverse = str.maketrans(
+    "",
+    "")
+
+str.translate("(2pMJ)", rot13-reverse)
+str.translate("uLLHKfhhLrnzKizvpJGKGsLipGzhyhpunFFryh19j3nsp4so01r5231438onson746q5r16\
+4o74j40LuJrnqiKxdHrhTrFrJnyktJGMHVql30qnpss3mr74om4433mo7pqm2045145265r5\
+&LrFnFLVql76n2nr5nm9s00m4s6om95rqm5q33q77p4q61", rot13-reverse)
