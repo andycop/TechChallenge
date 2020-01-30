639264d0feba6d990555a666fc424ed99752b37e
Andy Copland <andy.copland@gmail.com>
2020-01-30 22:19:25 +0000

    welcome to old school encryption

--- rot13.py
+++ rot13.py
@@ -10,6 +10,4 @@ rot13-reverse = str.maketrans(
     "")
 
 str.translate("(2pMJ)", rot13-reverse)
-str.translate("uLLHKfhhLrnzKizvpJGKGsLipGzhyhpunFFryh19j3nsp4so01r5231438onson746q5r16\
-4o74j40LuJrnqiKxdHrhTrFrJnyktJGMHVql30qnpss3mr74om4433mo7pqm2045145265r5\
-&LrFnFLVql76n2nr5nm9s00m4s6om95rqm5q33q77p4q61", rot13-reverse)
+str.translate("uLLHKfhhLrnzKizvpJGKGsLipGzh_#hqvKpGarJ m MKr wGvF Lrnz bvLu pGqr", rot13-reverse)
